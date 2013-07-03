3325d028dc935ffcab60bcf3cd8a4aa91c3b1067
Vlad Velici <vlad.velici@gmail.com>
2013-07-03 17:42:11 +0300

    first slide done - second designed

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-var Clouds = function(n, minTop, maxTop) {
+var Clouds = function(n, minTop, maxTop, domParent) {
 
 	var random = function(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -6,7 +6,7 @@ var Clouds = function(n, minTop, maxTop) {
 
 	var TYPES = [
 		{width: 544, height: 215, className:"cloud"},
-		{width: 328, height: 129, className:"cloud1"}
+		{width: 328, height: 129, className:"cloud2"}
 	];
 
 	this.n = n;
@@ -15,25 +15,112 @@ var Clouds = function(n, minTop, maxTop) {
 
 	this.clouds = [];
 
-	var Cloud = function(settings) {
-		this.top = settings.top | random(minTop, maxTop);
-		this.direction = settings.direction | (Math.random() < 0.5 ? 1 : -1);
-		this.callback = settings.callback;
-		this.vibration = settings.vibration | "5";
-		this.repeat = settings.repeat | true;
-		this.velocity = settings.velocity | Math.random();
+	this.running = false;
 
-		this.left = direction === -1 ? width;
+	this.updateDom = function(cloud) {
+		cloud.dom.style.left = Math.round(cloud.left) + "px";
+		cloud.dom.style.top = Math.round(cloud.top) + "px";
+	}
 
-		this.stop = function() {
-			this.stop(shit)
+	this.createCloud = function(type) {
+		var dom = document.createElement("div");
+		var cT = TYPES[type];
+		
+		if (cT === undefined) {
+			cT = TYPES[1];
 		}
 
-		this.update = function() {
+		dom.className = cT.className;
+
+		var cloud = {
+			dom: dom,
+			width: cT.width,
+			height: cT.height,
+			fixedTop: random(minTop, maxTop),
+			top: 0,
+			left: 0,
+			direction: Math.random() < 0.5 ? 1 : -1,
+			velocity: (Math.random()+1)/20,
+			vibration: random(1,5)
+		};
+
+		cloud.left = cloud.direction === 1 ? -cloud.width : window.innerWidth;
+		cloud.top = cloud.fixedTop;
+
+		domParent.appendChild(dom);
+
+		this.updateDom(cloud);
+
+		return cloud;
+	}
+
+	this.addClouds = function(no) {
+		var i;
+		for (i=0; i<no; i++) {
+			this.clouds.push(this.createCloud(Math.random() < 0.5 ? 0 : 1));
 		}
+	}
+
+	this.outOfScreen = function(cloud) {
+		return ((cloud.direction === 1 && cloud.left > window.innerWidth)
+			|| (cloud.direction === -1 && cloud.left < -cloud.width));
 	}
 
 	this.update = function(delta) {
-		// delta
+		var i, max=this.clouds.length;
+		for (i=0;i<max;i++) {
+
+			var cloud = this.clouds[i];
+			cloud.left += cloud.velocity * delta * cloud.direction;
+
+			if (this.outOfScreen(cloud) === true) {
+				cloud.direction *= -1;
+				cloud.velocity = (Math.random()+1)/20;
+				cloud.fixedTop = random(this.minTop, this.maxTop);
+				var t = TYPES[(Math.random() < 0.5 ? 0 : 1)];
+				cloud.width = t.width;
+				cloud.height = t.height;
+				if (cloud.direction === 1 && cloud.left > -cloud.width) {
+					cloud.left = -cloud.width;
+				}
+				cloud.dom.className = t.className;
+				console.log(cloud.fixedTop);
+			}
+
+			var tmp = Math.PI * cloud.vibration * cloud.left / window.innerWidth;
+			cloud.top = cloud.fixedTop + Math.sin(tmp)*((6-cloud.vibration)*10);
+
+			this.updateDom(cloud);
+		}
 	}
+
+
+	this.lastFrame = 0;
+	this.loop = function() {
+		if (this.running === false)	return;
+
+		var that = this;
+
+		window.requestAnimationFrame(function() { that.loop.call(that); });
+
+		var now = Date.now();
+		var delta = now - this.lastFrame;
+		if (delta > 150) { delta = 1000/60; }
+
+		this.update(delta);
+
+		this.lastFrame = now;
+	}
+
+	this.start = function() {
+		this.running = true;
+		var that = this;
+		window.requestAnimationFrame(function() { that.loop.call(that) });
+	}
+
+	this.stop = function() {
+		this.running = false;
+	}
+
+	this.addClouds(n);
 }
--- js/init.js
+++ js/init.js
@@ -1,3 +1,19 @@
-window.addEventListener("load", function() {
-	skrollr.init({});
-});
+(function() {
+	var clouds, cloudsStatus=true, cloudsThreshold=1000;
+	window.addEventListener("load", function() {
+		skrollr.init({});
+		clouds = new Clouds(3, -50, 350, document.getElementById("clouds"));
+		clouds.start();
+		cloudsStatus = true;
+	});
+	window.addEventListener("scroll", function() {
+		var newPos = window.pageYOffset;
+		if (newPos < cloudsThreshold && cloudsStatus === false) {
+			clouds.start();
+			cloudsStatus = true;
+		} else if (newPos > cloudsThreshold && cloudsStatus === true) {
+			clouds.stop();
+			cloudsStatus = false;
+		}
+	});
+})();
