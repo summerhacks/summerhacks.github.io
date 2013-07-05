3ceb9dcf1b02bcaae08ce911fc63fada9166c41e
Vlad Velici <vlad.velici@gmail.com>
2013-07-05 16:52:48 +0300

    menu fixed

--- js/init.js
+++ js/init.js
@@ -1,7 +1,9 @@
 (function() {
 	var clouds, cloudsStatus=true, cloudsThreshold=1000;
+	var skr;
 	window.addEventListener("load", function() {
-		skrollr.init({});
+		skr = skrollr.init({});
+		skrollr.menu.init(skr);
 		clouds = new Clouds(3, -50, 350, document.getElementById("clouds"));
 		clouds.start();
 		cloudsStatus = true;
