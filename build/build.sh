97679184b4632188a8b34b95c7257aea73ab1a61
Vlad Velici <vlad.velici@gmail.com>
2013-07-18 23:34:33 +0300

    Adds build functionality

---
+++ build/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# minify index.html
+java -jar htmlcompressor.jar --type html -o $1/index.html ../index.html
+
+# compile the css
+mkdir -p $1/css
+sass --style compressed ../css/main.sass:$1/css/styles.css
+
+# remove all <script> tags and add another one to js/summerhacks.js
+# sed -E 's/<script type=[^>]+><\/script>//g' ../index.html |
+# sed -E 's/<\/head>/<script src="\/js\/summerhacks.js"><\/script><\/head>/g' > $1/index.html
+
+# compile the javascripts one by one
+# mkdir -p $1/js
+
+# cat ../js/raf-shim.js > $1/js/tmp.js
+# cat ../js/background.js >> $1/js/tmp.js
+# cat ../js/skrollr.min.js >> $1/js/tmp.js
+# cat ../js/skrollr.menu.min.js >> $1/js/tmp.js
+# cat ../js/init.js >> $1/js/tmp.js
+
+# java -jar yuicompressor.jar $1/js/tmp.js > $1/js/summerhacks.js
+
+# clear temporary file
+# rm $1/js/tmp.js
+
+# fallback. copy javascripts
+cp -r ../js $1/
+
+# copy images
+cp -r ../images $1/
